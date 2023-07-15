7f91b3b82f352b20039a66d2aea633213839f3c7
parksangryong <ppsr3478@naver.com>
2023-07-15 22:10:54 +0900

    css

--- src/components/Grade.js
+++ src/components/Grade.js
@@ -4,10 +4,13 @@ import "../css/Grade.css";
 function Grade() {
   let scorelist = JSON.parse(localStorage.getItem("game"));
 
-  const result = scorelist.map((data) => <div>{data}</div>);
+  const result = scorelist.map((data) => (
+    <div className="data-score">{data}</div>
+  ));
   return (
     <div id="grade">
-      점수판
+      <div>점수판</div>
+      <hr></hr>
       <div>{result}</div>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import "../css/Home.css";
 function Home() {
   return (
     <div id="home">
-      <div onClick={() => (window.location.href = "/search")}>지도 속으로</div>
+      <div onClick={() => (window.location.href = "/search")}>지도 속으로!</div>
       <div onClick={() => (window.location.href = "/gamehome")}>
         국가 맞추기 게임!
       </div>
