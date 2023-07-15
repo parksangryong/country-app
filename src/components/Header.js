7473c7c81f8cf9fa6f7c16fa88c1a57e42851234
parksangryong <ppsr3478@naver.com>
2023-07-15 21:54:44 +0900

    버블정렬

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,10 +22,24 @@ function Header() {
 
   const getJson = () => {
     if (JSON.parse(localStorage.getItem("game")) !== null) {
-      let sortlist = JSON.parse(localStorage.getItem("game"));
-      sortlist.sort((a, b) => b - a);
+      let arr = JSON.parse(localStorage.getItem("game"));
 
-      setScorelist(sortlist);
+      var len = arr.length;
+      var swapped;
+
+      do {
+        swapped = false;
+        for (var i = 0; i < len - 1; i++) {
+          if (arr[i] < arr[i + 1]) {
+            var temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+            swapped = true;
+          }
+        }
+      } while (swapped);
+
+      setScorelist(arr);
     } else {
       return;
     }
